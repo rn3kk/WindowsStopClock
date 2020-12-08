d20727e9f435d87adbf71b770d6a55951f1d8ad9
Nikolay Banshchikov <rn3kk.nick@gmail.com>
2020-12-08 14:51:30 +0300

    Add files via upload

---
+++ WindowsStopClock.cpp
@@ -0,0 +1,28 @@
+﻿// WindowsStopClock.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <sysinfoapi.h>
+
+SYSTEMTIME currentTime;
+LPSYSTEMTIME lpTime;
+
+void stopTime()
+{
+    while (true)
+    {
+      SetSystemTime(lpTime);
+      std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    }
+}
+
+int main()
+{
+    lpTime = &currentTime;
+    GetSystemTime(lpTime);
+   
+    std::thread th(&stopTime);
+    th.join();
+}
